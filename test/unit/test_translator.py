fcd89ecf5db88d94b6a6ff5976c7051c24a8c018
Joyce Hong <joyce.hong8@gmail.com>
2024-04-01 17:15:12 -0400

    unit tests

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -4,10 +4,19 @@ from src.translator import translate_content
 def test_chinese():
     is_english, translated_content = translate_content("这是一条中文消息")
     assert is_english == False
-    assert translated_content == "This is a Chinese message"
+    assert "this is a chinese message" in translated_content.lower()
 
-def test_llm_normal_response():
-    pass
+def test_spanish():
+    is_english, translated_content = translate_content("Este es un mensaje Español")
+    assert is_english == False
+    assert "this is a spanish message" in translated_content.lower()
+
+def test_english():
+    is_english, translated_content = translate_content("This is an English message")
+    assert is_english == True
+    assert "This is an English message" == translated_content
 
 def test_llm_gibberish_response():
-    pass
+    is_english, translated_content = translate_content("adsfiagsdafjeoigh")
+    assert is_english == True
+    assert "adsfiagsdafjeoigh" in translated_content.lower()
